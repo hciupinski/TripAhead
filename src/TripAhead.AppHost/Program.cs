567aa5f49882a29dca46284f342a8fa3caeba717
Hubert Ciupinski <ithubertciupinski@gmail.com>
2024-03-02 08:43:19 +0100

    Task #1 Add layers for Trips and Reservations service

--- src/TripAhead.AppHost/Program.cs
+++ src/TripAhead.AppHost/Program.cs
@@ -1,8 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var tripsService = builder.AddProject<Projects.TripAhead_Trips_Api>("tripsservice");
+var reservationsService = builder.AddProject<Projects.TripAhead_Reservations_API>("reservationsservice");
 
 builder.AddProject<Projects.TripAhead_Web>("webfrontend")
-    .WithReference(tripsService);
+    .WithReference(tripsService)
+    .WithReference(reservationsService);
 
 builder.Build().Run();
