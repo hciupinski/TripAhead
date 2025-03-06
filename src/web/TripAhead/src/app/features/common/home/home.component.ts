f383d009b089dc00eebc554a079d81bd4edcdbd7
Hubert Ciupinski <ithubertciupinski@gmail.com>
2025-03-06 17:24:28 +0100

    Add missing files

---
+++ src/web/TripAhead/src/app/features/common/home/home.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthState} from "../../../core/store/auth/auth.reducer";
+import {Store} from "@ngrx/store";
+import {Observable} from "rxjs";
+import {initAuth, loadUser, logout} from "../../../core/store/auth/auth.actions";
+import {selectUserProfile} from "../../../core/store/auth/auth.selectors";
+import {CommonModule} from "@angular/common";
+import {User} from "../../../../types/user";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+  user$: Observable<User | null> | undefined;
+  loading$: Observable<boolean> | undefined;
+  error$: Observable<any> | undefined;
+constructor(private router: Router, private authState: Store<AuthState>) {
+  this.authState.dispatch(loadUser());
+
+  this.user$ = this.authState.select(selectUserProfile);
+}
+
+  ngOnInit(): void {
+    this.user$ = this.authState.select(selectUserProfile);
+  }
+
+  login(): void {
+    this.authState.dispatch(initAuth());
+  }
+
+  logout(): void {
+    this.authState.dispatch(logout());
+  }
+
+  navigateSecured(): void {
+        this.router.navigate(['secure']);
+  }
+
+  navigateNotSecured(): void {
+    this.router.navigate(['not-secure']);
+  }
+}
